fd72af5f989d416ea113107c7d38e68639b3b856
Megan Byers | Design Elixir <megan@designelixir.studio>
2022-04-19 14:25:26 -0600

    testing API

--- scripts/stepOne.js
+++ scripts/stepOne.js
@@ -1,12 +1,17 @@
 
 
 console.log("API CALLED -----------------------------------------------------------------");
+
 var url = "https://api.squarespace.com/1.0/commerce/orders?&fulfillmentStatus=PENDING";
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+xhr.open("GET", url, true);
 
 xhr.setRequestHeader("Authorization", "Bearer 2c247a47-b073-42e9-916a-549cf0b0aeed");
+xhr.setRequestHeader("User-Agent", "halp me");
+xhr.setRequestHeader("Access-Control-Allow-Origin", url);
+xhr.setRequestHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+xhr.setRequestHeader("Access-Control-Allow-Methods", "GET, POST");
 
 
 
@@ -14,7 +19,7 @@ xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log(xhr.status);
     //   console.log(xhr.responseText);
-      const daResponse = xhr.responseText;
+      var daResponse = xhr.responseText;
       console.log(daResponse);
    }};
 
